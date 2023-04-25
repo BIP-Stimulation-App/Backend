bfbed87713332b695d0d78bded7cd68aae476b00
Temptica <nicovsgaming@gmail.com>
2023-04-25 22:01:46 +0200

    Add Medication Class

---
+++ StimulationApp.DAL/Model/Medication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StimulationAppAPI.DAL.Model
+{
+    public class Medication
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public DateTime Next { get; set; } //exact day and time of next notification
+        public TimeSpan Frequency { get; set; } //how many minutes, hours etc shoudl be added after repeat
+        public string FrequencyText { get; set; }
+    }
+}
